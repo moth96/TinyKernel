a630d278abc3f8b92cb411d828a2cac8a295fe25
moth96 <312041716@qq.com>
2022-02-26 02:41:28 -0800

    Add lock

--- thread/thread.c
+++ thread/thread.c
@@ -105,7 +105,7 @@ void schedule() {
 
    ASSERT(intr_get_status() == INTR_OFF);
 
-   struct task_struct* cur = running_thread();
+   struct task_struct* cur = running_thread(); 
    if (cur->status == TASK_RUNNING) { // 若此线程只是cpu时间片到了,将其加入到就绪队列尾
       ASSERT(!elem_find(&thread_ready_list, &cur->general_tag));
       list_append(&thread_ready_list, &cur->general_tag);
@@ -123,6 +123,33 @@ void schedule() {
    struct task_struct* next = elem2entry(struct task_struct, general_tag, thread_tag);
    next->status = TASK_RUNNING;
    switch_to(cur, next);
+}
+
+/* 当前线程将自己阻塞,标志其状态为stat. */
+void thread_block(enum task_status stat) {
+/* stat取值为TASK_BLOCKED,TASK_WAITING,TASK_HANGING,也就是只有这三种状态才不会被调度*/
+   ASSERT(((stat == TASK_BLOCKED) || (stat == TASK_WAITING) || (stat == TASK_HANGING)));
+   enum intr_status old_status = intr_disable();
+   struct task_struct* cur_thread = running_thread();
+   cur_thread->status = stat; // 置其状态为stat 
+   schedule();		      // 将当前线程换下处理器
+/* 待当前线程被解除阻塞后才继续运行下面的intr_set_status */
+   intr_set_status(old_status);
+}
+
+/* 将线程pthread解除阻塞 */
+void thread_unblock(struct task_struct* pthread) {
+   enum intr_status old_status = intr_disable();
+   ASSERT(((pthread->status == TASK_BLOCKED) || (pthread->status == TASK_WAITING) || (pthread->status == TASK_HANGING)));
+   if (pthread->status != TASK_READY) {
+      ASSERT(!elem_find(&thread_ready_list, &pthread->general_tag));
+      if (elem_find(&thread_ready_list, &pthread->general_tag)) {
+	 PANIC("thread_unblock: blocked thread in ready_list\n");
+      }
+      list_push(&thread_ready_list, &pthread->general_tag);    // 放到队列的最前面,使其尽快得到调度
+      pthread->status = TASK_READY;
+   } 
+   intr_set_status(old_status);
 }
 
 /* 初始化线程环境 */
